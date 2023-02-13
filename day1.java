c51313bd5065fde32d48f634c7a69d2df8222f21
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-13 16:19:07 +0000

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,3 +1,17 @@
 public class day1 {
+    public static boolean isNarcissistic(int number) {
+        String num = String.valueOf(number);
+        int sum = 0;
+        boolean answer = false;
+        for(int i=0; i < num.length(); i++)
+        {
+            int j = Character.digit(num.charAt(i), 10);
+            sum = sum + (j^num.length());
+        }
+        System.out.println(sum="                  "+num)
     
+    
+        return answer;
+    }
+
 }
