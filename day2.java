0f87bb9807fe9556543f1c4905ffaa5a0e7fcd60
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-15 09:40:40 +0000

    Update day2.java
    Adding comments and finalizing the code

--- day2.java
+++ day2.java
@@ -8,6 +8,11 @@ public class day2 {
 		duplicateCount("BingBong");
 
 	}
+	/**
+	 * 
+	 * duplicateCount() takes a string and calculates the letters that recur on the string
+	 * This is achieved by using a for loop that parses through the String that is separated into characters that are stored in an arrayList
+	 */
 	public static void duplicateCount(String text) {
 	    // nested for loop with text.charAt(index)
 		ArrayList<String> textList = new ArrayList<String>();
@@ -15,14 +20,19 @@ public class day2 {
 		int dnumber= 0;
 		try
 		{
-			for(int index = 0; index < text.length(); index++)
+			for(int index = 0; index < text.length(); index++)// Separates the text into characters that can be sorted
 			{
-				char letter = Character.toLowerCase(text.charAt(index));
+				char letter = Character.toLowerCase(text.charAt(index)); // making all the characters lower case removes case sensitivity
 				textList.add(Character.toString(letter));
 				
 			}
 			
 			Collections.sort(textList);
+			/**
+			 * Using a for loop that compares current character and the next character. If the characters are equal to
+			 *  each other the number of duplicates increase as well as the number of the current duplicates.
+			 * Every time the characters do not match dnumber changes to 0, until the next group of duplicates is found.
+			 */
 			for(int out = 0; out < textList.size(); out++)
 			{
 				String curChar = textList.get(out);
@@ -47,7 +57,7 @@ public class day2 {
 				}
 			}
 		}
-		catch (IndexOutOfBoundsException e)
+		catch (IndexOutOfBoundsException e) // Due to nextChar getting out of bounds error handling is necessary.
 		{
 			;
 		}
