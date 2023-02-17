f1ce1d8c9b874d0dfcbb09c705ea613c06da52f0
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-17 16:27:41 +0000

    day 10 of challenges creation of a new JavaScript file
    Creation of the index.js and formulating the task and splitting into steps

---
+++ day 10/index.js
@@ -0,0 +1,4 @@
+// 1. number input     xyz
+// 2. number converted to string and seperated into characters [x,y,z]
+// 3. characters converted into singular int and multiplied x*y*z = ab
+// 4. repeat steps 2 and 3 until the answer is a single digit and record number of loops required to get to that point
