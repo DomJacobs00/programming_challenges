13b9df65146de81686b251414328360efee31237
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-18 18:21:48 +0000

    Create day11.js

---
+++ day11.js
@@ -0,0 +1,6 @@
+
+/**
+ * implementation of a function that accepts 3 integer values a,  b, c.
+ * The function should return true if a triangle can be build with the sides of given length and false in any other case.
+ * All triangles must have surface greater than 0 to be accepted.
+ */
