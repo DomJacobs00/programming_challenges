26a499dd9d33cae005b41b0902e8a76dcae1423f
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-18 19:01:37 +0000

    Update day11.js

--- day11.js
+++ day11.js
@@ -1,6 +1,28 @@
 
 /**
- * implementation of a function that accepts 3 integer values a,  b, c.
- * The function should return true if a triangle can be build with the sides of given length and false in any other case.
- * All triangles must have surface greater than 0 to be accepted.
+ * Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
  */
+function towerBuilder(nFloors)
+{
+    var floor = [];
+    var towerBlock = "*";
+    var len = 1;
+    var maxWidth = 2 * nFloors -1;
+    // using  a nested for loop building a tree
+    for (i = 0; i<nFloors; i++)
+    {
+        // for every floor 2 stars need to be added
+        for (k = 0; k<len; k++)
+        {
+            floor.push(towerBlock);
+        }
+        
+        var padding = " ".repeat((maxWidth - floor.length)/2);
+        var out = padding + floor.join('')+padding;
+        console.log(out);
+        len = len + 2;
+        floor = [];
+    }
+    
+}
+towerBuilder(3);
