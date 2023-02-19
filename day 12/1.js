4dcdfd1272f095dd32279e7e615fe1586c7a6af4
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-19 15:44:55 +0000

    Create 1.js
    Day 11 challenge

---
+++ day 12/1.js
@@ -0,0 +1,21 @@
+/**
+ * function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator
+ *  is the number of errors and the denominator the length of the control string. 
+ */
+
+function printerError(s)
+{
+    var goodColors = [a,b,c,d,e,f,g,h,i,j,k,l,m];
+    for(i = 0; i < s.length; i++)
+    {
+        for(k = 0; k<goodColors.length; k++)
+        {
+            if(s.charAt(i) == goodColors(k))
+            {
+                
+            }
+        }
+    }
+    var printer_errors = ""; 
+    return printer_errors;
+}
