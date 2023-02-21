779f05b4dc64aba7721d3e5e22dc484ee0d8f815
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-21 18:20:01 +0000

    Update day_14_1.js
    addition of max and current sum. Start of For loop

--- day 14/day_14_1.js
+++ day 14/day_14_1.js
@@ -1,10 +1,25 @@
+/**
+ * 
+ * The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+ * Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. 
+ * If the list is made up of only negative numbers, return 0 instead.
+ * Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+ */
+
+
+
 var maxSequence = function(arr){
-    var sum = 0;
-    for(i=0; i<arr.length; i++)
+    // if the array = 0
+    if(arr.length === 0)
     {
-        sum = sum+arr[i];
+        return 0;
     }
-    return sum;
+    let maxsum = 0;
+    let currentsum = 0;
+    for (i = 0; i < arr.length; i++)
+    {
+        
+    }
 
 
 
