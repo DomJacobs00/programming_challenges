df01a4413d4fee860c82310b4d55aff9ae86affd
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-23 19:32:25 +0000

    Finalized day 16 challenge, with added comments

--- day 16/Day_16.js
+++ day 16/Day_16.js
@@ -1,3 +1,14 @@
 /**
  * Finding a missing term in an Arithmetic progression
  */
+var findMissing = function (list)
+{
+    const n = list.length + 1;                                  // Calculate the total number of elements in the sequence, including the missing one                  
+    const a = list[0];                                          // Get the first element of the sequence
+    const b = list[list.length - 1];                            // Get the last element of the sequence
+    const totalSum = list.reduce((acc , val) => acc + val , 0); // Calculate the sum of all elements in the sequence
+    const expectedSum = ( n / 2) * ( a + b );                   // Calculate the sum of the sequence if no elements were missing
+    var ha =  expectedSum - totalSum;                           // Subtract the actual sum from the expected sum to get the missing element
+    return ha;
+}
+
