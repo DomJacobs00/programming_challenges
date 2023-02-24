a7debc51c22a274dbeffb5423a2d284b148d6d43
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-24 13:17:40 +0000

    Update day_17_1.js
    Attempt to understand  the task

--- day 17/day_17_1.js
+++ day 17/day_17_1.js
@@ -1,3 +1,20 @@
 /**
  * A chain adding function
  */
+
+var numbers = "";
+function add(n)
+{
+    return function(m)
+    {
+        return add(n+m);
+    }
+}
+function sum()
+{
+    let result = 0;
+    for()
+}
+
+add(1);
+
